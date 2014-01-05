e01a631d8c7e52683102f60d964621d355eb554b
Oliver Faircliff <olls@ovi.com>
2014-01-05 00:00:55 +0000

    Rewritten vector and square functions, created box and vector_p functions

--- shapes.py
+++ shapes.py
@@ -2,58 +2,89 @@ import math
 
 """ Functions to generate different shapes to be used as sprite images. """
 
-def vector(angle, length):
-    """
-        angle in degrees
-        length in pixels
-    """
-    xLength = length * math.sin(math.radians(angle))
-    yLength = length * math.cos(math.radians(angle))
-    image = [[False for x in range(abs(int(xLength))+1)] for y in range(abs(int(yLength))+1)]
-
-    if (angle-45) % 180 < 90:
-
-        if xLength <= 0: # If -ve length --> go backwards
-            for xOff in range(0, int(xLength), -1):
-                image[int((xLength - xOff) / math.tan(math.radians(angle)))]\
-                     [int(xLength - xOff)] = True
-
-        else:
-            
-            for xOff in range(0, int(xLength), 1):
-                image[int((xLength - xOff) / math.tan(math.radians(angle)))]\
-                     [int(xLength - xOff)] = True
+def vector_p(y, x):
+    image = [[False for xPos in range(abs(x)+1)] 
+             for yPos in range(abs(y)+1)]
     
+    yMirror = False
+    xMirror = False
+    if y < 0:
+        yMirror = True
+    if x < 0:
+        xMirror = True
+
+    y0 = 0
+    x0 = 0
+    y1 = abs(y)
+    x1 = abs(x)
+
+    dy = abs(y1-y0) 
+    dx = abs(x1-x0)
+
+    if y0 < y1:
+        sy = 1
     else:
+        sy = -1
+    if x0 < x1:
+        sx = 1
+    else:
+        sx = -1
 
-        if yLength <= 0: # If -ve length --> go backwards
-            for yOff in range(0, int(yLength), -1):
-                image[int(yLength - yOff)]\
-                     [int((yLength - yOff) * math.tan(math.radians(angle)))] = True
+    err = dx-dy
+    while not (y0 == y1 and x0 == x1):
+        image[y0][x0] = True
+        e2 = 2*err
 
-        else:
-            for yOff in range(0, int(yLength), 1):
-                image[int(yLength - yOff)]\
-                     [int((yLength - yOff) * math.tan(math.radians(angle)))] = True
-    
+        if e2 > -dy:
+            err -= dy
+            x0 += sx
+        
+        if x0 == x1 and y0 == y1:
+            image[y0][x0] = True
+            break
+        
+        if e2 < dx:
+            err += dx
+            y0 += sy
+
+    if yMirror:
+        image.reverse()
+    if xMirror:
+        for row in image:
+            row.reverse()
+
     return image
+
+def vector(angle, length):
+    xLength = int(length * math.sin(math.radians(angle)))
+    yLength = int(length * math.cos(math.radians(angle)))
+
+    return vector_p(yLength, xLength)
 
 def square(size):
     """
         size = (width, height) in pixels
-        the char to use
+    """
+    return [[False for x in range(size[0])] for y in range(size[1])]
+
+def box(size):
+    """
+        size = (width, height) in pixels
     """
     image = [[False for x in range(size[0])] for y in range(size[1])]
-
     for xPos in range(0, size[0]):
-        for yPos in range(0, size[1]):
-            image[yPos][xPos] = True
+        image[0][xPos] = True
+        image[size[1]][xPos] = True
+
+    for yPos in range(0, size[1]):
+        image[yPos][0] = True
+        image[yPos][size[1]] = True
+
     return image
 
 def circle(radius):
     """
         radius in pixels
-        the char to use
     """
     image = [[False for x in range(radius*2+1)] 
              for y in range(radius*2+1)]
