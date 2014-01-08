068b3ce07b923b0c2a4a18c4bc3f7156b436ba49
Oliver Faircliff <olls@ovi.com>
2014-01-08 10:40:08 +0000

    Added avg. FPS conunt to clock

--- clock.py
+++ clock.py
@@ -36,27 +36,33 @@ def main():
     screen.addSprite(minute)
     screen.addSprite(hour)
 
-    while True:
-        t = int(time.time())
+    frames = 0
+    start = time.time()
+    try:
+        while True:
+            t = int(time.time())
 
-        for hand, secPerRev in [(second, 60), (minute, 3600), (hour, 43200)]:
+            for hand, secPerRev in [(second, 60), (minute, 3600), (hour, 43200)]:
 
-            angle = ((t * (360 / secPerRev)) % 360)+90
-            hand.img.setAngle(angle)
+                angle = ((t * (360 / secPerRev)) % 360)+90
+                hand.img.setAngle(angle)
 
-            width, height = hand.img.width, hand.img.height
+                width, height = hand.img.width, hand.img.height
 
-            if angle > 90 and angle <= 180:
-                hand.setPos((center-height, center))
-            elif angle > 180 and angle <= 270:
-                hand.setPos((center, center))
-            elif angle > 270 and angle <= 360:
-                hand.setPos((center, center-width))
-            elif angle > 360 and angle <= 450:
-                hand.setPos((center-height, center-width))
+                if angle > 90 and angle <= 180:
+                    hand.setPos((center-height, center))
+                elif angle > 180 and angle <= 270:
+                    hand.setPos((center, center))
+                elif angle > 270 and angle <= 360:
+                    hand.setPos((center, center-width))
+                elif angle > 360 and angle <= 450:
+                    hand.setPos((center-height, center-width))
 
-        print(screen, end='')
-        time.sleep(0.05)
+            print(screen, end='')
+            time.sleep(0.05)
+            frames += 1
+    except KeyboardInterrupt:
+        print('\nAvg FPS: '+ str( frames / (time.time()-start) ))
 
 if __name__ == '__main__':
     main()
