1d6ff2183c8f0a543545f228508f5626f186de60
Oliver Faircliff <olls@ovi.com>
2014-01-22 10:44:21 +0000

    Added a new circle pattern.

---
+++ circle2.py
@@ -0,0 +1,39 @@
+import time
+import copy
+
+import graphics as g
+
+screen = g.Canvas( size=(21, 21) )
+
+circle = g.Sprite(
+    g.shapes.Circle( 0, filled = True ),
+    (10, 10)
+)
+screen.sprites.append( circle )
+
+i = 0
+
+while True:
+
+
+    radius = circle.image.radius
+    if radius == 0:
+        radius = 10
+        while screen.sprites:
+            screen.sprites.pop()
+            print( screen )
+            time.sleep( .04 )
+    else:
+        radius -= 1
+
+    circle = copy.deepcopy(circle)
+    circle.image.radius = radius
+    circle.position = (10 - radius, 10 - radius)
+    circle.color += 1
+    circle.color %= 8
+    screen.sprites.append(circle)
+
+    print( screen )
+    time.sleep( .04 )
+
+    i += 1
