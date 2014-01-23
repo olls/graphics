f7f502d875915665c5383c12d2448a64ec09f211
Oliver Faircliff <olls@ovi.com>
2014-01-23 20:03:40 +0000

    Made example.py pep8 compliant. #32

--- example.py
+++ example.py
@@ -2,23 +2,24 @@ import time
 
 import graphics as g
 
+
 def main():
-    screen = g.Canvas( size=(20, 20) )
+    screen = g.Canvas(size=(20, 20))
 
     line = g.Sprite(
-        g.shapes.Vector( 0, 10 ),
+        g.shapes.Vector(0, 10),
         (5, 5)
     )
-    screen.sprites.append( line )
+    screen.sprites.append(line)
 
     with g.NonBlockingInput() as nbi:
         while True:
 
             if nbi.char() == ' ':
                 line.image.angle += 5
-                print( screen )
+                print(screen)
 
-            time.sleep( .01 )
+            time.sleep(.01)
 
 if __name__ == '__main__':
     main()
