efd5fd8a272da428a375774bedbde76be8611c76
Oliver Faircliff <olls@ovi.com>
2014-01-23 19:23:36 +0000

    Made circle.py pep8 compliant. #32

--- circle.py
+++ circle.py
@@ -1,13 +1,13 @@
 import graphics as g
 import time
 
-screen = g.Canvas( size=(21, 21) )
+screen = g.Canvas(size=(21, 21))
 
 circle = g.Sprite(
-    g.shapes.Circle( 0, filled = True ),
+    g.shapes.Circle(0, filled=True),
     (10, 10)
 )
-screen.sprites.append( circle )
+screen.sprites.append(circle)
 
 circleDir = True
 i = 0
@@ -26,9 +26,9 @@ while True:
         circleDir = True
 
     circle.position = (10 - radius, 10 - radius)
-    circle.color = int( i /4 ) %6
+    circle.color = int(i / 4) % 6
 
-    print( screen )
-    time.sleep( .04 )
+    print(screen)
+    time.sleep(.04)
 
     i += 1
