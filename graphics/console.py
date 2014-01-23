5aefb77a8908c1a1f8cef83ecb95501b932228b4
Oliver Faircliff <olls@ovi.com>
2014-01-23 16:52:35 +0000

    Made console.py pep8 compliant. #32

--- graphics/console.py
+++ graphics/console.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Size:
     def __init__(self):
         self.method = 1
@@ -16,19 +17,23 @@ class Size:
 
     def getSize(self):
         if self.method == 1:
-            cr = Size.ioctl_GWINSZ( 0 ) or Size.ioctl_GWINSZ( 1 ) or Size.ioctl_GWINSZ( 2 )
+            cr = Size.ioctl_GWINSZ(0) or \
+                 Size.ioctl_GWINSZ(1) or \
+                 Size.ioctl_GWINSZ(2)
         elif self.method == 2:
-            fd = os.open( os.ctermid(), os.O_RDONLY )
-            cr = Size.ioctl_GWINSZ( fd )
-            os.close( fd )
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = Size.ioctl_GWINSZ(fd)
+            os.close(fd)
         elif self.method == 3:
-            cr = ( self.env.get( 'LINES', 25 ), self.env.get( 'COLUMNS', 80 ) )
+            cr = (self.env.get('LINES', 25), self.env.get('COLUMNS', 80))
 
-        return int( cr[1] ), int( cr[0] )
+        return int(cr[1]), int(cr[0])
 
-    def ioctl_GWINSZ( fd ):
+    def ioctl_GWINSZ(fd):
         try:
-            import fcntl, termios, struct, os
+            import fcntl
+            import termios
+            import struct
             cr = struct.unpack(
                 'hh',
                 fcntl.ioctl(
@@ -40,6 +45,7 @@ class Size:
         except:
             return
         return cr
+
 
 def main():
     size = Size()
