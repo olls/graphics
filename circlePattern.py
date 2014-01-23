387895429859c15e753aa8d59f7932c75a36c67d
Oliver Faircliff <olls@ovi.com>
2014-01-23 20:02:44 +0000

    Made circlePattern.py pep8 compliant. #32

--- circlePattern.py
+++ circlePattern.py
@@ -1,19 +1,21 @@
 import random as rand
 import graphics as g
 
+
 def main():
     print('Please Wait...')
-    canvas = g.Canvas( fullscreen = True )
+    canvas = g.Canvas(fullscreen=True)
 
     for circles in range(200):
+        size = rand.randint(1, int(min(canvas.width / 4, canvas.height / 2)))
         circle = g.Sprite(
-            g.shapes.Circle( rand.randint(1, int(min(canvas.width/4, canvas.height/2))), filled = True ),
-            position = ( rand.randint(0, canvas.width-1),
-                         rand.randint(0, canvas.height-1) )
+            g.shapes.Circle(size, filled=True),
+            position = (rand.randint(0, canvas.width - 1),
+                        rand.randint(0, canvas.height - 1))
         )
-        canvas.sprites.append( circle )
-        if circle.edge( canvas ) or circle.touching( canvas ):
-            canvas.sprites.remove( circle )
+        canvas.sprites.append(circle)
+        if circle.edge(canvas) or circle.touching(canvas):
+            canvas.sprites.remove(circle)
 
     print(canvas)
 
