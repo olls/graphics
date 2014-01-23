07811109a33dc35140bd282372788727be6e9e97
Oliver Faircliff <olls@ovi.com>
2014-01-23 16:56:31 +0000

    Made colors.py pep8 compliant. #32

--- graphics/colors.py
+++ graphics/colors.py
@@ -1,27 +1,28 @@
 import sys
 
-BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = list(range( 8 ))
 
-def has_colors( stream ):
-	if not hasattr( stream, 'isatty' ):
-		return False
-	if not stream.isatty():
-		return False
-	try:
-		import curses
-		curses.setupterm()
-		return curses.tigetnum( 'colors' ) > 2
-	except:
-		return False
+def has_colors(stream):
+    if not hasattr(stream, 'isatty'):
+        return False
+    if not stream.isatty():
+        return False
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum('colors') > 2
+    except:
+        return False
 
-has_colors = has_colors( sys.stdout )
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = list(range(8))
+has_colors = has_colors(sys.stdout)
 
-def colorStr( text, color=WHITE ):
-	if has_colors:
-		seq = '\x1b[1;%dm' % ( 30 + color ) + text + '\x1b[0m'
-		return seq
-	else:
-		return text
+
+def colorStr(text, color=WHITE):
+    if has_colors:
+        seq = '\x1b[1;%dm' % (30 + color) + text + '\x1b[0m'
+        return seq
+    else:
+        return text
 
 if __name__ == '__main__':
-	print( colorStr( 'hi', CYAN ) )
+    print(colorStr('hi', CYAN))
