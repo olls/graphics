1f678180ecb80cc3a7617c7105f21b20f8fd9f75
Oliver Faircliff <olls@ovi.com>
2014-01-23 16:43:07 +0000

    Made canvas.py almost pep8 compliant. #32

--- graphics/canvas.py
+++ graphics/canvas.py
@@ -5,6 +5,7 @@ from . import colors
 from . import console
 from . import shapes
 
+
 class Canvas:
     """
         size = (int width, int height)
@@ -12,12 +13,12 @@ class Canvas:
         center = bool
         border = bool
     """
-    def __init__( self,
-                  size = (40, 30),
-                  fullscreen = False,
-                  background = ' ',
-                  center = True,
-                  border = True ):
+    def __init__(self,
+                 size = (40, 30),
+                 fullscreen = False,
+                 background = ' ',
+                 center = True,
+                 border = True):
 
         self.center = center
         self.border = border
@@ -33,8 +34,7 @@ class Canvas:
 
         self.sprites = []
 
-
-    def __str__( self ):
+    def __str__(self):
         """
             Returns the screen as a string, taking
                 the Canvas attributes into account.
@@ -46,8 +46,8 @@ class Canvas:
 
         # Generate screen
         if isinstance(self.background, str):
-            display = [[ str(self.background) for x in range( self.width ) ]
-                         for y in range( self.height )]
+            display = [[str(self.background) for x in range(self.width)]
+                        for y in range(self.height)]
         else:
             display = copy.deepcopy(self.background)
 
@@ -55,29 +55,31 @@ class Canvas:
         for sprite in self.sprites:
 
             image = sprite.image.image()
-            for y, row in enumerate( image ):
-                for x, pixel in enumerate( row ):
+            for y, row in enumerate(image):
+                for x, pixel in enumerate(row):
                     if pixel:
-                        pixelPos = [ int( sprite.position[1] + y ), int( sprite.position[0] + x ) ]
+                        pixelPos = [int(sprite.position[1] + y),
+                                    int(sprite.position[0] + x)]
 
                         # Test if pixel position is on canvas.
                         visible = True
                         try:
-                            display[ pixelPos[0] ][ pixelPos[1] ]
+                            display[pixelPos[0]][pixelPos[1]]
                         except IndexError:
                             visible = False
                             pass
 
                         if visible:
-                            char = colors.colorStr( sprite.char((x, y)), sprite.color )
-                            display[ pixelPos[0] ][ pixelPos[1] ] = char
+                            char = colors.colorStr(sprite.char((x, y)),
+                                                   sprite.color)
+                            display[pixelPos[0]][pixelPos[1]] = char
 
         hPad = (
             self.center * (
-                int(( s[0] -
-                       ( (self.width * 2) -1 ) -
-                       ( 4* self.border )
-                    )/2 ) * ' '
+                int((s[0] -
+                       ((self.width * 2) - 1) -
+                       (4 * self.border)
+                    ) / 2) * ' '
             )
         )
 
@@ -85,75 +87,77 @@ class Canvas:
         return (
             # Top padding:
             '\n\n' +
-            ( '\n' * self.border ) +
+            ('\n' * self.border) +
 
             # Top border:
             (self.border * (
                 hPad +
                 '╭' +
-                ( '─' * ( (self.width *2) +1) ) +
+                ('─' * ((self.width * 2) + 1)) +
                 '╮\n'
             )) +
 
             # First line padding
             hPad +
-            ( self.border * '│ ' ) +
+            (self.border * '│ ') +
             (
                 (
                     # Between each line
-                    ( self.border * ' │' ) +
+                    (self.border * ' │') +
                     '\n' +
                     hPad +
-                    ( self.border * '│ ' )
+                    (self.border * '│ ')
                 ).join(
-                    [ ' '.join(row) for row in display ]
+                    [' '.join(row) for row in display]
                 )
             ) +
-            ( self.border * ' │' ) +
+            (self.border * ' │') +
 
             # Bottom border:
             (self.border * (
                 '\n' +
                 hPad +
                 '╰' +
-                ('─' * ( (self.width *2) +1) ) +
+                ('─' * ((self.width * 2) + 1)) +
                 '╯'
             )) +
 
             # Bottom padding:
             (self.center * (
-                '\n' * int(( s[1] -
-                             ( self.height + (2 * self.border) )
-                           ) /2 )
+                '\n' * int((s[1] -
+                            (self.height + (2 * self.border))
+                           ) / 2)
             ))
         )
 
-    def testPixel( self, testPixel ):
+    def testPixel(self, testPixel):
         """
             testPixel = ( int x, int y )
         """
         for sprite in self.sprites:
-            for y, row in enumerate( sprite.image.image() ):
-                for x, pixel in enumerate( row ):
-                    if pixel and testPixel[0] == sprite.position[0]+x and\
-                                  testPixel[1] == sprite.position[1]+y:
+            for y, row in enumerate(sprite.image.image()):
+                for x, pixel in enumerate(row):
+                    if pixel and testPixel[0] == sprite.position[0] + x and\
+                                  testPixel[1] == sprite.position[1] + y:
                             return True
         return False
 
-    def updateSize( self ):
+    def updateSize(self):
         s = self.size.getSize()
-        return int((s[0]/2)-5), int(s[1]-5)
+        return int((s[0] / 2) - 5), int(s[1] - 5)
 
     @property
-    def border( self ):
+    def border(self):
         return self._border
+
     @border.setter
-    def border( self, b ):
-        self._border = bool( b )
+    def border(self, b):
+        self._border = bool(b)
 
     @property
-    def center( self ):
+    def center(self):
         return self._center
+
     @center.setter
-    def center( self, c ):
-        self._center = bool( c )
+    def center(self, c):
+        self._center = bool(c)
