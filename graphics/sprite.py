1b0842050f40157871db8ee731512b23d835918a
Oliver Faircliff <olls@ovi.com>
2014-01-23 16:48:57 +0000

    Made sprite.py pep8 compliant. #32

--- graphics/sprite.py
+++ graphics/sprite.py
@@ -4,20 +4,21 @@ import copy
 from . import colors
 from . import shapes
 
+
 class Sprite:
-    def __init__( self, image, position=(0, 0), color=None, char=None ):
+    def __init__(self, image, position=(0, 0), color=None, char=None):
         """
             image = Image() instance
-            position = ( int x, int y )
-            color = int range( 0, 8 )
+            position = (int x, int y)
+            color = int range(0, 8)
             char = str char
         """
 
-        self.image = copy.deepcopy( image )
-        self.position = [ int( position[0] ), int( position[1] ) ]
+        self.image = copy.deepcopy(image)
+        self.position = [int(position[0]), int(position[1])]
 
-        self.color = color if color else random.randint( 1, 8 )
-        self._char = str(char)[0] if char else chr( 0x25CF )
+        self.color = color if color else random.randint(1, 8)
+        self._char = str(char)[0] if char else chr(0x25CF)
 
     def char(self, pos):
         try:
@@ -28,9 +29,9 @@ class Sprite:
             char = self._char
         return char
 
-    def move( self, direction=0 ):
+    def move(self, direction=0):
         """
-            direction = int range( 0, 4 )
+            direction = int range(0, 4)
 
             0 = Down
             1 = Left
@@ -49,7 +50,7 @@ class Sprite:
         elif direction == 3:
             self.position[0] += 1
 
-    def touching( self, canvas, side=None ):
+    def touching(self, canvas, side=None):
         """
             Returns True if touching any pixels [on specified side].
 
@@ -74,58 +75,58 @@ class Sprite:
         height = len(image)
         if 0 in side:
             # Find bottom edges
-            for x in range( width ):
-                if image[-1][x] == True:
+            for x in range(width):
+                if image[-1][x]:
                     y = height
                 else:
-                    y = height-1
-                    while image[y][x] == False:
+                    y = height - 1
+                    while not image[y][x]:
                         y -= 1
                     y += 1
-                edges.append( (x, y) )
+                edges.append((x, y))
         if 1 in side:
             # Find left edges
-            for y in range( height ):
-                if image[y][0] == True:
+            for y in range(height):
+                if image[y][0]:
                     x = -1
                 else:
                     x = 0
-                    while image[y][x] == False:
+                    while not image[y][x]:
                         x += 1
                     x -= 1
-                edges.append( (x, y) )
+                edges.append((x, y))
         if 2 in side:
             # Find top edges
-            for x in range( width ):
-                if image[0][x] == True:
+            for x in range(width):
+                if image[0][x]:
                     y = -1
                 else:
                     y = 0
-                    while image[y][x] == False:
+                    while not image[y][x]:
                         y += 1
                     y -= 1
-                edges.append( (x, y) )
+                edges.append((x, y))
         if 3 in side:
             # Find right edges
-            for y in range( height ):
-                if image[y][-1] == True:
+            for y in range(height):
+                if image[y][-1]:
                     x = width
                 else:
-                    x = width-1
-                    while image[y][x] == False:
+                    x = width - 1
+                    while not image[y][x]:
                         x -= 1
                     x += 1
-                edges.append( (x, y) )
+                edges.append((x, y))
 
         # Find if any other sprites are in our edge coords.
         for pixel in edges:
-            pixel = ( pixel[0] + int( self.position[0] ),
-                      pixel[1] + int( self.position[1] ) )
-            if canvas.testPixel( pixel ):
+            pixel = (pixel[0] + int(self.position[0]),
+                     pixel[1] + int(self.position[1]))
+            if canvas.testPixel(pixel):
                 return True
         return False
 
-    def edge( self, canvas ):
+    def edge(self, canvas):
         """
             Returns a list of the sides of the sprite
                 which are touching the edge of the canvas.
@@ -136,16 +137,16 @@ class Sprite:
             3 = Right
         """
         sides = []
-        if int( self.position[0] ) <= 0:
-            sides.append( 1 )
+        if int(self.position[0]) <= 0:
+            sides.append(1)
 
-        if ( int( self.position[0] ) + self.image.width ) >= canvas.width:
-            sides.append( 3 )
+        if (int(self.position[0]) + self.image.width) >= canvas.width:
+            sides.append(3)
 
-        if int( self.position[1] ) <= 0:
-            sides.append( 2 )
+        if int(self.position[1]) <= 0:
+            sides.append(2)
 
-        if ( int( self.position[1] ) + self.image.height ) >= canvas.height:
-            sides.append( 0 )
+        if (int(self.position[1]) + self.image.height) >= canvas.height:
+            sides.append(0)
 
         return sides
