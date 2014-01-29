818483821f8639f9766ce55e0458b8220239b956
Oliver Faircliff <olls@ovi.com>
2014-01-29 11:16:57 +0000

    Removed old code in nbinput

--- graphics/nbinput.py
+++ graphics/nbinput.py
@@ -5,21 +5,6 @@ import termios
 import time
 
 
-# class NonBlockingInput(object):
-#     def __enter__(self):
-#         self.old_settings = termios.tcgetattr(sys.stdin)
-#         tty.setcbreak(sys.stdin.fileno())
-#         return self
-
-#     def __exit__(self, type, value, traceback):
-#         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
-
-#     def char(self):
-#         if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
-#             return sys.stdin.read(1)
-#         return False
-
-
 class NonBlockingInput:
     """
     Gets a single character from standard input.  Does not echo to the
