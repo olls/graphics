977b75423991f15b38f87e2734f2f25d5d546c7b
Oliver Faircliff <mail@oliverfaircliff.com>
2016-02-10 17:48:52 +0000

    Version 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='graphics',
-      version='1.0',
+      version='1.1',
       description='A console based graphics engine for simple Unicode games and animations. ',
       url='http://github.com/olls/graphics',
       author='Oliver Faircliff',
