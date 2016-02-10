9ab30e96d4489e833d4f0137b5f255c160486f54
Oliver Faircliff <mail@oliverfaircliff.com>
2016-02-10 17:40:16 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graphics',
+      version='1.0',
+      description='A console based graphics engine for simple Unicode games and animations. ',
+      url='http://github.com/olls/graphics',
+      author='Oliver Faircliff',
+      author_email='mail@oliverfaircliff.com',
+      license='GPL3',
+      packages=['graphics'],
+      zip_safe=False)
